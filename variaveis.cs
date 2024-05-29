f9679935d64a4ca4a6b26537196b3dd6c620bfe8
kevin alencar <kevinalencar169@gmail.com>
2024-05-29 16:12:19 -0300

    criando o protocolo ftp. e configurando a aba cadastro cliente

--- variaveis.cs
+++ variaveis.cs
@@ -12,10 +12,16 @@ namespace fitnessfusion
         public static string funcao;
         public static int LinhaSelecionada;
         //MENU
-        
+
         //CLIENTES
 
         //FUNCIONARIOS
+
+        //ESTRUTURA FTP
+        public static string enderecoServidorFtp = "smpsistema.com.br";
+        public static string usuarioFtp = "u283879542.fitnessfusion";
+        public static string senhaFtp = "Senac@fitnessfusion01";
+        //fim estrutura
 
     }
 }
